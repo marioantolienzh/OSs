6703395e7fc0180ca211b203f616f9f8dedeea8f
marioantolienzh <50441834+marioantolienzh@users.noreply.github.com>
2022-02-09 09:05:24 -0500

    Create pthread_mutex.c

---
+++ pthread_mutex.c
@@ -0,0 +1,33 @@
+#include <pthread.h>
+#include <stdio.h>
+
+static int count = 0;
+static pthread_mutex_t countlock = PTHREAD_MUTEX_INITIALIZER;
+int increment(void) { /* increment the counter */
+ int error;
+ if (error == pthread_mutex_lock(&countlock))
+ return error;
+ count++;
+ return pthread_mutex_unlock(&countlock);
+}
+int decrement(void) { /* decrement the counter */
+ int error;
+ if (error == pthread_mutex_lock(&countlock))
+ return error;
+ count--;
+ return pthread_mutex_unlock(&countlock);
+}
+int getcount(int *countp) { /* retrieve the counter */
+ int error;
+ if (error == pthread_mutex_lock(&countlock))
+ return error;
+ *countp = count;
+ return pthread_mutex_unlock(&countlock);
+}
+int main() {
+  increment();
+  increment();
+  increment();
+  getcount(&count);
+  printf("%d\n",count);
+}
